0977e427172eefe87835f3c3cb9b914e24fabdda
batistaevcr-design <batistaevcr@gmail.com>
2026-06-04 23:08:45 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'ticos-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html', './manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
